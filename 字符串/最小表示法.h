3335c19368fc74b2e82fc05362094dc4e61e43b8
Yangyu Chen <cyy@cyyself.name>
2018-08-15 11:43:16 +0800

    Create 最小表示法.h

---
+++ 字符串/最小表示法.h
@@ -0,0 +1,15 @@
+int strmin(char *s) {
+	int i=0,j=1,k=0;
+	int len = strlen(s);
+	while (i < len && j < len && k < len) {
+		int d = s[(i+k)%len] - s[(j+k)%len];
+		if (d == 0) k ++;
+		else {
+			if (d > 0) i = i + k + 1;
+			else j = j + k + 1;
+			if (i == j) j ++;
+			k = 0;
+		}
+	}
+	return min(i,j);
+}
