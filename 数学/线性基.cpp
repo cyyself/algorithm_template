f018a828f11384ca7c0279e19edf3a989ec0c45f
Yangyu Chen <cyy@cyyself.name>
2019-07-20 00:30:54 +0800

    Update 线性基.cpp

--- 数学/线性基.cpp
+++ 数学/线性基.cpp
@@ -1,24 +1,52 @@
 //给定n个整数（数字可能重复），求在这些数中选取任意个，使得他们的异或和最大。
 #include <bits/stdc++.h>
 using namespace std;
-long long a[65];
+struct LB {
+	long long r[63];
+	LB() {
+		memset(r,0,sizeof(r));
+	}
+	LB(const LB &b) {
+		for (int i=0;i<63;i++) r[i] = b.r[i];
+	}
+	bool insert(long long x) {
+		for (int i=62;i>=0;i--) {
+			if ((x >> i) & 1) {
+				if (r[i] == 0) {
+					r[i] = x;
+					return true;
+				}
+				else x ^= r[i];
+			}
+		}
+		return false;
+	}
+	bool check(long long x) {
+		for (int i=62;i>=0;i--) {
+			if ((x >> i) & 1) {
+				if (r[i] == 0) return false;
+				x ^= r[i];
+			}
+		}
+		return true;
+	}
+	long long query_max(long long ret = 0) {//自定义初始值
+		for (int i=62;i>=0;i--) ret = max(ret,ret^r[i]);
+		return ret;
+	}
+	void merge(const LB &b) {
+		for (int i=0;i<=62;i++) insert(b.r[i]);
+	}
+};
 int main() {
+	LB A;
 	int n;
 	scanf("%d",&n);
-	for (int i=0;i<n;i++) {
-		long long tmp;
-		scanf("%lld",&tmp);
-		for (int j=log(tmp)/log(2);j>=0;j--) 
-			if (tmp & (1LL << j)) {
-				if (a[j]) tmp ^= a[j];
-				else {
-					a[j] = tmp;
-					break;
-				}
-			}
+	while (n --) {
+		long long x;
+		scanf("%lld",&x);
+		A.insert(x);
 	}
-	long long ans = 0;
-	for (int i=60;i>=0;i--) ans = max(ans,ans^a[i]);
-	printf("%lld\n",ans);
+	cout << A.query_max() << endl;
 	return 0;
 }
