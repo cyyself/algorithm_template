1fbc64caa93af583e628556a280e05c9f8559e1f
Yangyu Chen <cyy@cyyself.name>
2019-08-04 13:36:27 +0800

    Update Dijkstra费用流.cpp

--- 图论/网络流/Dijkstra费用流.cpp
+++ 图论/网络流/Dijkstra费用流.cpp
@@ -1,28 +1,33 @@
-const int INF = 0x3f3f3f3f;
+const long long INF = 0x3f3f3f3f3f3f3f3f;
 struct edge {
-	int to, capacity, cost, rev;
+	int to;
+	long long capacity, cost;
+	int rev;
 	edge() {}
-	edge(int to, int _capacity, int _cost, int _rev) :to(to), capacity(_capacity), cost(_cost), rev(_rev) {}
+	edge(int to, long long _capacity, long long _cost,int _rev) :to(to), capacity(_capacity), cost(_cost), rev(_rev) {}
 };
 struct Min_Cost_Max_Flow {
-	int V, H[4005], dis[4005], PreV[4005], PreE[4005];
+	int V;
+	long long H[4005], dis[4005];
+	int PreV[4005], PreE[4005];
 	vector<edge> G[4005];
-	void Init(int n) {
+	void init(int n) {
 		V = n;
 		for (int i = 0; i <= V; ++i)G[i].clear();
 	}
-	void Add_Edge(int from, int to, int cap, int cost) {
+	void addedge(int from, int to, long long cap, long long cost) {
 		G[from].push_back(edge(to, cap, cost, G[to].size()));
 		G[to].push_back(edge(from, 0, -cost, G[from].size() - 1));
 	}
-	int Min_cost_max_flow(int s, int t, int f, int& flow) {
-		int res = 0; fill(H, H + 1 + V, 0);
+	long long cal(int s, int t, long long f, long long& flow) {
+		long long res = 0;
+		fill(H, H + 1 + V, 0);
 		while (f) {
-			priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > q;
+			priority_queue <pair<long long, int>, vector<pair<long long, int> >, greater<pair<long long, int> > > q;
 			fill(dis, dis + 1 + V, INF);
 			dis[s] = 0; q.push(pair<int, int>(0, s));
 			while (!q.empty()) {
-				pair<int, int> now = q.top(); q.pop();
+				pair<long long, int> now = q.top(); q.pop();
 				int v = now.second;
 				if (dis[v] < now.first)continue;
 				for (int i = 0; i < G[v].size(); ++i) {
@@ -31,13 +36,13 @@ struct Min_Cost_Max_Flow {
 						dis[e.to] = dis[v] + e.cost + H[v] - H[e.to];
 						PreV[e.to] = v;
 						PreE[e.to] = i;
-						q.push(pair<int, int>(dis[e.to], e.to));
+						q.push(pair<long long, int>(dis[e.to], e.to));
 					}
 				}
 			}
 			if (dis[t] == INF)break;
 			for (int i = 0; i <= V; ++i)H[i] += dis[i];
-			int d = f;
+			long long d = f;
 			for (int v = t; v != s; v = PreV[v])d = min(d, G[PreV[v]][PreE[v]].capacity);
 			f -= d; flow += d; res += d*H[t];
 			for (int v = t; v != s; v = PreV[v]) {
