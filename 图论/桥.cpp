6cdfebbdbe4ce320a42ff9123044cdc3fd526f35
Yangyu Chen <cyy@cyyself.name>
2019-10-19 14:00:43 +0800

    Update 桥.cpp

--- 图论/桥.cpp
+++ 图论/桥.cpp
@@ -1,47 +1,76 @@
-//给一张无向图，求图中的割边数量
-#include <bits/stdc++.h>
+//http://poj.org/problem?id=3177
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
 using namespace std;
-int dfn[10005];
-int low[10005];
-vector <int> g[10005];
-map <pair<int,int>,int> cnt;
-int ans;
-int n,m;
+const int maxn = 10005;
+const int maxm = 20005;
+struct DATA {
+	int v,next;
+}e[20005];
+int ecnt;
+int head[maxn];
+void addedge(int u,int v) {
+	e[ecnt].v = v;
+	e[ecnt].next = head[u];
+	head[u] = ecnt;
+	ecnt ++;
+}
+int dfn[maxn],low[maxn];
 int ts;
+bool br[maxm];
 void tarjan(int u,int r) {
 	dfn[u] = low[u] = ++ts;
-	for (auto v:g[u]) {
+	for (int i=head[u];~i;i=e[i].next) {
+		int v = e[i].v;
 		if (v == r) continue;
 		if (!dfn[v]) {
 			tarjan(v,u);
-			if (low[v] > dfn[u] && cnt[make_pair(u,v)] == 1) ans ++;
+			if (low[v] > dfn[u]) {
+				br[ i ] = true;
+				br[i^1] = true;
+			}
 			low[u] = min(low[u],low[v]);
 		}
 		else low[u] = min(low[u],dfn[v]);
 	}
 }
-int main() {
-	int T;
-	scanf("%d",&T);
-	for (int cas=1;cas<=T;cas++) {
-		ans = 0;
-		ts = 0;
-		cnt.clear();
-		scanf("%d%d",&n,&m);
-		for (int i=1;i<=n;i++) {
-			dfn[i] = low[i] = 0;
-			g[i].clear();
-		}
-		for (int i=0;i<m;i++) {
-			int u,v;
-			scanf("%d%d",&u,&v);
-			g[u].push_back(v);
-			g[v].push_back(u);
-			cnt[make_pair(u,v)] ++;
-			cnt[make_pair(v,u)] ++;
-		}
-		for (int i=1;i<=n;i++) if (!dfn[i]) tarjan(i,i);
-		printf("Case %d: %d\n",cas,ans);
+int rt[maxn],subg;
+int deg[maxn];
+void dfs(int u) {
+	rt[u] = subg;
+	for (int i=head[u];~i;i=e[i].next) {
+		if (br[i]) continue;
+		int v = e[i].v;
+		if (!rt[v]) dfs(v);
 	}
+}
+int main() {
+	memset(head,-1,sizeof(head));
+	int f,r;
+	scanf("%d%d",&f,&r);
+	for (int i=0;i<r;i++) {
+		int u,v;
+		scanf("%d%d",&u,&v);
+		addedge(u,v);
+		addedge(v,u);
+	}
+	for (int i=1;i<=f;i++) if (!dfn[i]) tarjan(i,i);
+	for (int i=1;i<=f;i++) if (!rt[i]) {
+		subg ++;
+		dfs(i);
+	}
+	for (int u=1;u<=f;u++) {
+		for (int i=head[u];~i;i=e[i].next) {
+			int v = e[i].v;
+			if (rt[u] != rt[v]) {
+				deg[rt[u]] ++;
+				deg[rt[v]] ++;
+			}
+		}
+	}
+	int cnt = 0;
+	for (int i=1;i<=subg;i++) if (deg[i] == 2) cnt ++;
+	printf("%d\n",(cnt+1)/2);
 	return 0;
 }
