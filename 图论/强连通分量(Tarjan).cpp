f828ab0b286947d18f428c895b6e1cb4d016e4be
Yangyu Chen <cyy@cyyself.name>
2019-11-09 20:42:40 +0800

    Update 强连通分量(Tarjan).cpp

--- 图论/强连通分量(Tarjan).cpp
+++ 图论/强连通分量(Tarjan).cpp
@@ -1,87 +1,79 @@
-//UVa 247:https://vjudge.net/problem/UVA-247
-#include <bits/stdc++.h>
+//POJ 2186，奶牛的仰慕具有传递性，给m个仰慕关系，问多少奶牛被所有的奶牛仰慕
+#include <cstdio>
+#include <cstring>
+#include <stack>
 using namespace std;
-map <string,int> name;
-map <int,string> name2;
-vector <int> g[30];
-bool vis[30];
-int dfn[30];
-int low[30];
-int root[30];
-int rootcnt;
-int timestamp;
+const int maxn = 10005;
+const int maxe = 50005;
+int dfn[maxn],low[maxn],rt[maxn],sz[maxn],ts,scc;
+bool vis[maxn];
 stack <int> s;
+int head[maxn];
+struct Edge {
+	int v,next;
+}e[maxe];
+int ecnt;
+void addedge(int u,int v) {
+	e[ecnt].v = v;
+	e[ecnt].next = head[u];
+	head[u] = ecnt;
+	ecnt ++;
+}
 void tarjan(int u) {
-	dfn[u] = low[u] = ++timestamp;
+	dfn[u] = low[u] = ++ts;
 	vis[u] = true;
 	s.push(u);
-	for (auto v:g[u]) {
+	for (int i=head[u];~i;i=e[i].next) {
+		int v = e[i].v;
 		if (!dfn[v]) {
 			tarjan(v);
 			low[u] = min(low[u],low[v]);
 		}
-		else {
-			if (vis[v]) low[u] = min(low[u],dfn[v]);
-		}
+		else if (vis[v]) low[u] = min(low[u],dfn[v]);
 	}
 	if (dfn[u] == low[u]) {
-		rootcnt ++;
+		scc ++;
 		while (!s.empty()) {
 			int v = s.top();
 			s.pop();
-			root[v] = rootcnt;
+			rt[v] = scc;
+			sz[scc] ++;
 			vis[v] = false;
 			if (v == u) break;
 		}
 	}
 }
+int outdeg[maxn];
 int main() {
 	int n,m;
-	int c = 1;
-	while (cin >> n >> m) {
-		if (n == 0 && m == 0) break;
-		for (int i=1;i<=n;i++) g[i].clear();
-		memset(root,0,sizeof(root));
-		memset(vis,false,sizeof(vis));
-		memset(dfn,0,sizeof(dfn));
-		memset(low,0,sizeof(low));
-		timestamp = 0;
-		rootcnt = 0;
-		int namecnt = 0;
-		name.clear();
-		name2.clear();
-		while (m --) {
-			string a,b;
-			cin >> a >> b;
-			int _a,_b;
-			if (name[a] == 0) {
-				name[a] = ++namecnt;
-				name2[namecnt] = a;
-			}
-			if (name[b] == 0) {
-				name[b] = ++namecnt;
-				name2[namecnt] = b;
-			}
-			_a = name[a];
-			_b = name[b];
-			g[_a].push_back(_b);
+	while (scanf("%d%d",&n,&m) == 2) {
+		memset(head,-1,sizeof(head));
+		ecnt = 0;
+		ts = 0;
+		scc = 0;
+		for (int i=1;i<=n;i++) dfn[i] = low[i] = rt[i] = sz[i] = 0;
+		for (int i=0;i<m;i++) {
+			int u,v;
+			scanf("%d%d",&u,&v);
+			addedge(u,v);
 		}
-		if (c > 1) cout << endl;
-		cout << "Calling circles for data set "<< c++ <<":\n";
 		for (int i=1;i<=n;i++) if (!dfn[i]) tarjan(i);
-		memset(vis,false,sizeof(vis));
-		for (int i=1;i<=n;i++) {
-			if (vis[i]) continue;
-			vis[i] = true;
-			cout << name2[i];
-			for (int j=i+1;j<=n;j++) {
-				if (root[i] == root[j]) {
-					cout << ", " << name2[j];
-					vis[j] = true;
-				}
+		for (int i=1;i<=scc;i++) outdeg[i] = 0;
+		for (int u=1;u<=n;u++) {
+			int uu = rt[u];
+			for (int i=head[u];~i;i=e[i].next) {
+				int v = e[i].v;
+				int vv = rt[v];
+				if (uu != vv) outdeg[uu] ++;
 			}
-			cout << endl;
 		}
+		int ans = -1;
+		for (int i=1;i<=scc;i++) if (outdeg[i] == 0) {
+			if (ans == -1) ans = i;
+			else ans = 0;
+		}
+		if (ans > 0) printf("%d\n",sz[ans]);
+		else printf("0\n");
 	}
 	return 0;
 }
