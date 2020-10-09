6b6e2075160ce4128d22ebb27a6c1ed26391740c
Yangyu Chen <cyy@cyyself.name>
2020-10-09 16:37:57 +0800

    optimize checker

--- 交互器/test.py
+++ 交互器/test.py
@@ -7,14 +7,17 @@ print("(debug)","ans=",ans)
 for i in range(20):
 	out = int(s.stdout.readline().decode("utf-8"))
 	print("debug",out)
-	if int(out) == ans:
-		s.stdin.write(str(0).encode("utf-8"))
+	outbuf = ""
+	if out == ans:
+		outbuf = str(0)
+		print("Check Passed")
 		break
 	elif int(out) < ans:
-		s.stdin.write(str(-1).encode("utf-8"))
+		outbuf = str(-1)
 	else:
-		s.stdin.write(str(1).encode("utf-8"))
-	s.stdin.write("\n".encode("utf-8"))
+		outbuf = str(1)
+	outbuf += "\n"
+	s.stdin.write(outbuf.encode("utf-8"))
 	s.stdin.flush()
 	print("(debug)","i=",i)
 s.kill()
